5073234cd094104333731f902accf4a9e164a3da
sakshamgoyal01 <126040462+sakshamgoyal01@users.noreply.github.com>
2024-03-10 13:21:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+def generate_password(length = 12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
+
+
+if __name__ == '__main__':
+    password_length = int(input("enter desired password length="))
+
+    if password_length < 1:
+        print("password length must be atleast 1")
+
+    else:
+        generated_password = generate_password(password_length)
+        print("generated password:",generated_password)
